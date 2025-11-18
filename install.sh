12ef0dc6a948501ff9684b21c239c4496bfc7e8f
iluvgirlswithglasses <luunamdat204@gmail.com>
2025-11-18 19:41:43 +0700

    use uv by default (less prompting and error-free)

--- install.sh
+++ install.sh
@@ -6,32 +6,19 @@ git clone https://github.com/iluvgirlswithglasses/linux-desktop-gremlin ~/.confi
 echo "Clone completed!"
 cd ~/.config/linux-desktop-gremlin
 
-# Ask user about virtual environment setup
-echo "Would you like to set this up with a virtual environment [Y/n]"
-read -r choice_vir_env
-choice_vir_env=${choice_vir_env:-Y} # Default to "yes" if empty input
-choice_lower=$(echo "$choice_vir_env" | tr '[:upper:]' '[:lower:]')
-
-if [[ "$choice_lower" == "y" || "$choice_lower" == "" ]]; then
-	# Check if uv is installed
-	if command -v uv >/dev/null 2>&1; then
-		echo "uv is installed"
-	else
-		echo "uv is not installed, installing..."
-		echo "Executing: curl -LsSf https://astral.sh/uv/install.sh | sh"
-		curl -LsSf https://astral.sh/uv/install.sh | sh
-	fi
-
-	echo "Running 'uv sync' to install required packages..."
-	uv sync
+# Install uv and sync
+if command -v uv >/dev/null 2>&1; then
+    echo "uv is installed"
 else
-	echo "Please install pyside6/pyside6-tools and qt6-base using your package manager."
-	echo "The gremlins need them :3"
+    echo "uv is not installed, installing..."
+    echo "Executing: curl -LsSf https://astral.sh/uv/install.sh | sh"
+    curl -LsSf https://astral.sh/uv/install.sh | sh
 fi
 
+echo "Running 'uv sync' to install required packages..."
+uv sync
 
 # Variables
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 INSTALL_PATH="$HOME/.config/linux-desktop-gremlin"
 BIN_PATH="$HOME/.local/bin"
 LINK_PATH="$BIN_PATH/gremlin-picker"
